466b971c18b480657c9e302afeec27b90d992538
oBrunoz <alencarb98@gmail.com>
2023-02-19 16:46:36 -0300

    #

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://alencarbdev:Alencar@21@blogapp-prod.6bufnl0.mongodb.net/?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://alencarbdev:Alencar%4021@blogapp-prod.6bufnl0.mongodb.net/?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://127.0.0.1/blogapp"}
 }
