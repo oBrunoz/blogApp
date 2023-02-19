16a8ba3289ef19f89455db5d5721f2d049c6ba8c
oBrunoz <alencarb98@gmail.com>
2023-02-19 16:51:30 -0300

    #

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(bodyparser.json());
 mongoose.set('strictQuery', true);
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log(`Conectado ao Mongo!`);
     } catch(err) {
         console.log(err);
